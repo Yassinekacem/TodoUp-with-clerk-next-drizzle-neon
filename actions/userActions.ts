0e0c9fd13bffab8fe67df7e41edc5f3e3065900c
Yassinekacem <kacemyassine740@gmail.com>
2024-07-27 00:00:53 +0100

    first commit

--- actions/userActions.ts
+++ actions/userActions.ts
@@ -25,10 +25,14 @@ export const getAllUsers = async () => {
 };
 
 
-export const addUser = async () => {
+export const addUser = async (user : any) => {
   await db.insert(users).values({ 
-    name : "user1",
-    email : "yassine@gmail.com",
+    clerkId : user?.clerkId,
+    email : user?.email,
+    name : user?.name,  
+    firstName : user?.firstName,  
+    lastName : user?.lastName,
+    photo : user?.photo
   });
-  revalidatePath("/");
+  // revalidatePath("/");
 };
