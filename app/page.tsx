653840f79c9e7624b5575934602f1c2d11b65194
Yassinekacem <kacemyassine740@gmail.com>
2024-07-27 12:18:37 +0100

    relation btwn CurrentUser and todo

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import { getData } from "@/actions/todoActions";
 import { getAllUsers, getUser } from "@/actions/userActions";
 import Todos from "@/components/Todos";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
 export default async function Home() {
-  // const users = await getAllUsers();
-  // console.log(users)
-  // const data = await getData(users[0].id);
-  // const user = await getUser(users[0].id);
-  // console.log(user)
+  const user : any = await currentUser()
+  if (!user) return 
+  const fetchData = await getUser(user?.id)
+  console.log(fetchData)
   return (
+    fetchData && (
     <main className="flex   items-center justify-between ">
-      {/* <Todos todos={data} user={users[0]} /> */}
-    </main>
+      <Todos todos={fetchData[0].todos } user={fetchData[0]} />
+    </main> )
   );
 }
--- components/Todos.tsx
+++ components/Todos.tsx
@@ -17,8 +17,8 @@ const Todos: FC<Props> = ({ todos , user }) => {
 
   // Function to create a new todo item
   const createTodo = (text: string) => {
-    // addUser()
-    const id = (todoItems.at(-1)?.id || 0) + 1;
+    // addUser() 
+const  id = new Date().getTime();
     addTodo(id, text , user?.id);
     setTodoItems((prev) => [...prev, { id: id, text, done: false , userId : user?.id }]);
   }; 
@@ -48,7 +48,7 @@ const Todos: FC<Props> = ({ todos , user }) => {
   // Rendering the Todo List component
   return (
     <main className="flex mx-auto max-w-xl w-full min-h-screen flex-col items-center p-16">
-      <div className="text-5xl font-medium">To-do app</div>
+      <div className="text-5xl font-medium">My Todos </div>
       <div className="w-full flex flex-col mt-8 gap-2">
         {/* Mapping through todoItems and rendering Todo component for each */}
         {todoItems.map((todo) => (
